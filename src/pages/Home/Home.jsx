c4ec3a7b232a4f503151417bf6d4021747a3eef8
maxi-cor <ecornalba@estudiolevex.com.ar>
2024-06-19 17:14:53 -0300

    settings experience scroll

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,11 @@ import profile from '../../assets/profile.png';
 import levex from '../../assets/levex.png';
 import realState from '../../assets/realState.png';
 import marianoAcosta from '../../assets/marianoAcosta.png';
+import lvx from '../../assets/lvx.png';
+import noCountry from '../../assets/noCountry.png';
+import henry from '../../assets/henry.png';
+
+
 import { NavLink } from 'react-router-dom';
 
 const Home = () => {
@@ -60,7 +65,7 @@ const Home = () => {
                 <p>At Levex, a distributor of AVEVA software in Latin America, I am responsible for website maintenance. My tasks include creating new sections with updated information, adapting the website to design changes, developing internal tools for staff, and adapting the company's app for events.</p>
                 <div className='container-buttons-project'>
                   <div className='button-projects'>
-                    <NavLink aria-current="page" to="/projects"  className="projectClass">Project</NavLink>
+                    <NavLink aria-current="page" to="/projects" className="projectClass">Project</NavLink>
                   </div>
                   <div className='button-projects'>
                     <a href="https://www.levexsa.com/" target="_blank" rel="noopener noreferrer">Site</a>
@@ -76,7 +81,7 @@ const Home = () => {
                 <p>Using Webflow, a project was created to develop a real estate website, incorporating 3D technology via Spline to offer a dynamic and attractive visual experience.</p>
                 <div className='container-buttons-project'>
                   <div className='button-projects'>
-                    <NavLink aria-current="page" to="/projects"  className="projectClass">Project</NavLink>
+                    <NavLink aria-current="page" to="/projects" className="projectClass">Project</NavLink>
                   </div>
                   <div className='button-projects'>
                     <a href="https://maximilianos-real-estate-web.webflow.io/" target="_blank" rel="noopener noreferrer">Site</a>
@@ -84,7 +89,7 @@ const Home = () => {
                 </div>
               </div>
             </div>
-            
+
             <div class="project-item">
               <div className='container-box'>
                 <h4>SITE MARIANO ACOSTA</h4>
@@ -92,7 +97,7 @@ const Home = () => {
                 <p>For the Mariano Acosta Social and Sports Club, an integral project was implemented that encompassed the adaptation of multiple disciplines, SEO optimization, and the design of an intuitive interface with creative graphics. This improved navigation and communication with the club's members.</p>
                 <div className='container-buttons-project'>
                   <div className='button-projects'>
-                    <NavLink aria-current="page" to="/projects"  className="projectClass">Project</NavLink>
+                    <NavLink aria-current="page" to="/projects" className="projectClass">Project</NavLink>
                   </div>
                   <div className='button-projects'>
                     <a href="https://club-mariano-acosta.vercel.app/" target="_blank" rel="noopener noreferrer">Site</a>
@@ -106,10 +111,50 @@ const Home = () => {
         <div className='container-experience'>
           <h2>EXPERIENCE</h2>
           <div class="experience">
-            <p>• Experiencia 1</p>
-            <p>• Experiencia 2</p>
-            <p>• Experiencia 3</p>
+
+            <div className='experience-item'>
+              <div className='contaner-box-experience'>
+                <img src={lvx} alt='icon levex' />
+                <div className='container-text-box-experience'>
+                  <h4>Levex</h4>
+                  <p>Front-End Developer</p>
+                  <p>Sept. 2023 - present</p>
+                  <p>Responsible for the maintenance and continuous development of the company's website. Responsible for the strategic incorporation of content, as well as the efficient execution of periodic updates. I ensure optimal site performance by implementing the latest web practices and technologies, thereby contributing to improved online presence and user experience.</p>
+                </div>
+              </div>
+            </div>
+
+            <div className='experience-item'>
+              <div className='contaner-box-experience'>
+                <img src={noCountry} alt='icon levex' />
+                <div className='container-text-box-experience'>
+                  <h4>Levex</h4>
+                  <p>Front-End Developer</p>
+                  <p>Sept. 2022 - Sept. 2023</p>
+                  <p>● Development of E-commerce, using agile methodologies (SCRUM).</p>
+                  <p>● Front-end development of the site, designing components, styling.</p>
+                  <p>● Contribution to the back-end development of the application.</p>
+                </div>
+              </div>
+            </div>
+
+            <div className='experience-item'>
+              <div className='contaner-box-experience'>
+                <img src={henry} alt='icon levex' />
+                <div className='container-text-box-experience'>
+                  <h4>Henry - Bootcamp</h4>
+                  <p>Full-Stack Developer</p>
+                  <p>May. 2022 - Sept. 2022</p>
+                  <p>● Complete the bootcamp with (+800 hours). TECH SKILLS: ECMAScript 6, React, Redux, Node.js, Express.js, HTML, Algorithms and complexity analysis, Data structures, Sequelize, PostgreSQL, GIT.</p>
+                  <p>● Develop the app for the client and server side using React, Redux, HTML, JavaScript, Tailwind, TypeScript, Node.js, Sequelize, PostgreSQL, BCrypt, JWT, among others.</p>
+                </div>
+              </div>
+            </div>
+
           </div>
+
+
+
         </div>
       </section>
     </>
